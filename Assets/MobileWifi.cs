51fe82d12d339d28b2468f39a00eb3c5084205ab
Andreas Reich <andreasr@unity3d.com>
2019-01-27 14:24:58 +0100

    fix incorrect spawn reset on startup

--- Assets/MobileWifi.cs
+++ Assets/MobileWifi.cs
@@ -15,7 +15,8 @@ public class MobileWifi : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Respawn();
+        Wifi.SetActive(false);
+        _balance = StartPrepaidBalance;
     }
 
     // Update is called once per frame
